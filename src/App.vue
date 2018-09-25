40249c636c5e363f350d7cb192eaf38c46506195
Wouter Roosendaal <wouter@yellenge.nl>
2018-09-25 11:39:28 +0200

    TSify App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <v-app light>
     <v-toolbar class="white">
       <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn v-if="showHome" to="/" flat>Home</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <router-view></router-view>
@@ -10,14 +14,18 @@
 </template>
 <script lang="ts">
 import Vue from 'vue';
+import Component from 'vue-class-component';
 
-export default Vue.extend({
-  name: 'App',
-  data() {
-    return {
-      title: 'Yolo(go)',
-    };
-  },
+const AppProps = Vue.extend({})
 
-});
+@Component({})
+export default class App extends AppProps {
+  get title () {
+    return `Yolo (${this.$route.name})`;
+  }
+  get showHome () {
+    return this.$route.name != 'home'
+  }
+
+}
 </script>
