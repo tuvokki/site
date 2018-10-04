2eb54c068e2d33731803669e44e502f43b284d71
Wouter <tuvokki@gmail.com>
2018-10-04 08:00:52 +0200

    Add todo to store

--- src/store.ts
+++ src/store.ts
@@ -5,12 +5,8 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    baseURL: 'http://localhost:8000/',
+    baseURL: 'http://localhost:8000/', // TODO: Fix for production
   },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  mutations: {},
+  actions: {},
 });
